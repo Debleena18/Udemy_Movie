94fcc49a0c947100dedd44225d1b8836cfb62577
Debleena18 <sarkardebleena18@gmail.com>
2022-04-19 12:17:01 +0530

    useEffect and useCallback

--- src/App.js
+++ src/App.js
@@ -1,44 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
-//Here we use Fetch API, it returns a promise, that is JS language feature
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
- async function fetchMoviesHandler() {
-  setIsLoading(true);
-  setError(null);
-  try {
-    const responce = await fetch('https://swapi.dev/api/films/')
-    if (!responce.ok) {
-      throw new Error("Something is wrong")
-    }
+//Since useEffect has a func as dependency ans as we know in JS func is treated as obj and ehen this component is re-evaluated 
+//the func gets created again and again so to prevent this we use a hook called as useCallback.
+//For details go to section 12 of udemy course on Complete React Guide.
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
 
-    const data = await responce.json();
-    
-  //Since in Backend i.e abouve link the names of few props are different so we need to transform names.
-          const transformedMovies = data.results.map((movieData) => {
-            return {
-              id: movieData.episode_id,
-              title: movieData.title,
-              openingText: movieData.opening_crawl,
-              releaseDate: movieData.release_date,
-            };
-          });
-          setMovies(transformedMovies);
-          setIsLoading(false);
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
     }
-    catch (error){
-      setError(error.message)
-  }
-  setIsLoading(false)
-}
- 
-let content = <p>Found no movies.</p>;
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  let content = <p>Found no movies.</p>;
 
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
